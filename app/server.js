bdc7beabdf6c9f4b7d689575ec89e97962c64600
Nana Janashia <nn_janashia@yahoo.com>
2021-01-11 10:24:22 +0100

    final adjustments

---
+++ app/server.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let path = require('path');
+let fs = require('fs');
+let app = express();
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get('/profile-picture', function (req, res) {
+  let img = fs.readFileSync(path.join(__dirname, "images/profile.jpg"));
+  res.writeHead(200, {'Content-Type': 'image/jpg' });
+  res.end(img, 'binary');
+});
+
+app.listen(3000, function () {
+  console.log("app listening on port 3000!");
+});
+
