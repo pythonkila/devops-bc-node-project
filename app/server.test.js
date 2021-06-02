4a2c6294a8206010304a66c6d933f8782fa3955d
Nana Janashia <nn_janashia@yahoo.com>
2021-06-02 15:45:12 +0200

    Fix test

--- app/server.test.js
+++ app/server.test.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const fs = require('fs')
 
 test('main index.html file exists', () => {
-  expect(path.join(__dirname, "index.html")).toBeTruthy();
+  const filePath = path.join(__dirname, "index.html")
+  expect(fs.existsSync(filePath)).toBeTruthy();
 });
